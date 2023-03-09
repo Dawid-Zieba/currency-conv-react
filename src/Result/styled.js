3c0163253321f369a07cea38b1b4bbb35855ca2a
Dawid Zieba <d.zieba2@gmail.com>
2023-03-09 13:08:09 +0100

    changed css to js

---
+++ src/Result/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledParagraph = styled.p`
+  text-align: center;
+  font-size: 25px;
+  padding: 20px;
+  font-weight: 600;
+  color: black;
+`;
