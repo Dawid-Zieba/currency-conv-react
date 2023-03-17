ed665824156aeb6a1f80d7106697002d55a518c6
Dawid Zieba <d.zieba2@gmail.com>
2023-03-17 10:19:04 +0100

    pinned API from exchange.host

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,65 +1,99 @@
-
-import { currencies } from "../currencies";
 import { useState } from "react";
 import Result from "../Result";
 import { Clock } from "../Clock";
-import { StyledForm, StyledDiv, Fieldset,  StyledSelect, Legend, LabelText, Field } from "./styled";
+import { Button ,StyledForm, StyledDiv, Fieldset, StyledSelect, Legend, LabelText, Field, StyledParagraph, StyledParagraphLoading, StyledParagraphFailure } from "./styled";
+import { useRatesData } from "./useRatesData";
 
 
 const Form = () => {
-
+    const [currency, setCurrency] = useState("EUR");
     const [amount, setAmount] = useState("");
-    const [currency, setCurrency] = useState(currencies[0].short);
-    const rate = currencies.find(({ short }) => short === currency).rate;
+    const [result, setResult] = useState();
+
+    const ratesData = useRatesData();
+
+    const calculateResult = (currency, amount) => {
+        const rate = ratesData.rates[currency];
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount * rate,
+            currency,
+        });
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    };
 
     return (
-
-        <StyledForm>
+        <StyledForm onSubmit={onSubmit}>
             <StyledDiv>
-            <Clock />
-                <Legend>Wymiana waluty</Legend>
-                <Fieldset>
-                <p>
-                    <label>
-                        <LabelText>Kwota w PLN</LabelText>
-                        <Field
-                            type="number"
-                            step="0.01"
-                            required min="0"
-                            placeholder="Wpisz kwotę"
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                        />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <LabelText>Waluta</LabelText>
-                        <StyledSelect
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                        >
-                            {currencies.map((currency) => (
-                                <option
-                                    value={currency.short}
-                                    key={currency.short}
-                                >
-                                    {currency.name}
-                                </option>
-                            ))}
-                        </StyledSelect>
-                    </label>
-                </p>
-
-                <Result
-                    amount={amount}
-                    currency={currency}
-                    rate={rate}
-                />
-            </Fieldset>
+                <p></p>
+                <Clock />
+                    <Legend>
+                        Kalkulator walutowy
+                    </Legend>
+                        {ratesData.state === "loading"
+                            ? (
+                                <StyledParagraphLoading>
+                                    Chwileczkę, <br /> ładuję kursy walut z Europejskiego Banku Centralnego.
+                                </StyledParagraphLoading>
+                             ) 
+                             : (
+                        ratesData.state === "error"
+                            ? (
+                                <StyledParagraphFailure>
+                                    Strona nie załadowała się. Sprawdź połączenie internetowe.
+                                </StyledParagraphFailure>
+                            ) : (
+                                <>
+                                    <Fieldset>
+                                        <p>
+                                            <LabelText>
+                                                Wpisz kwotę w PLN:
+                                            </LabelText>
+                                            <Field
+                                                type="number"
+                                                step="0.01"
+                                                required min="0"
+                                                placeholder="Wpisz kwotę"
+                                                value={amount}
+                                                onChange={({ target }) => setAmount(target.value)}
+                                            />
+                                            <label>
+                                            <p></p>
+                                                <LabelText>
+                                                    Wybierz walutę:
+                                                </LabelText>
+                                                <StyledSelect
+                                                    value={currency}
+                                                    onChange={({ target }) => setCurrency(target.value)}
+                                                >
+                                                    {Object.keys(ratesData.rates).map(((currency) => (
+                                                    <option
+                                                        value={currency}
+                                                        key={currency}
+                                                >
+                                                    {currency}
+                                                </option>
+                                                )))}
+                                                </StyledSelect>
+                                            </label>
+                                        </p>
+                                        <Button>Przelicz</Button>
+                                        <StyledParagraph>Kurs walut pochodzi z Europejskiego Banku Centralnego z dnia: <br></br><strong>{ratesData.date}</strong></StyledParagraph>
+                                        <Result
+                                            result={result}
+                                            />
+                                    </Fieldset>
+                               </>
+                            )
+                        )}
             </StyledDiv>
+                                                
         </StyledForm>
     );
 };
-
 export default Form;
